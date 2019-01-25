a80bba6cfbb92931274c24b548ca88ca0c68d997
ashishkumarguptaak <ashishg00485@gmail.com>
2019-01-26 02:39:15 +0530

    TEST

--- src/app/home/image/images.ts
+++ src/app/home/image/images.ts
@@ -5,46 +5,46 @@ export const IMAGES: Image[] = [
         id: '0',
         name: 'YOUNG BEAUTIFUL GIRL',
         //image: 'https://isonomous-ground.000webhostapp.com/HackGallery/ownerimage2.jpg'
-        image: '../../../assets/test.pdf'
+        image: '../../../assets/icons/pdf.gif'
     },
     {
         id: '1',
         name: 'JUST BEAUTIFUL',
-        image: '../../../assets/ownerimage2.jpg'
+        image: '../../../assets/icons/pdf.gif'
     },
     {
         id: '2',
         name: 'FLOWERS NATURE',
-        image: '../../../assets/ownerimage3.jpg'
+        image: '../../../assets/icons/pdf.gif'
     },
     {
         id: '3',
         name: 'YOSEMITE',
-        image: '../../../assets/ownerimage4.jpg'
+        image: '../../../assets/icons/pdf.gif'
         },
     {
         id: '4',
         name: 'MOUNTAIN VIEW PANORAMA',
-        image: '../../../assets/ownerimage5.jpg'
+        image: '../../../assets/icons/pdf.gif'
         },
     {
         id: '5',
         name: 'TRENDY GUYS',
-        image: '../../../assets/ownerimage6.jpg'
+        image: '../../../assets/icons/pdf.gif'
         },
     {
         id: '6',
         name: 'PHOTO TAKING',
-        image: '../../../assets/ownerimage7.jpg'
+        image: '../../../assets/icons/pdf.gif'
     },
     {
         id: '7',
         name: 'GORGEOUS GIRL ON SEA',
-        image: '../../../assets/ownerimage8.jpg'
+        image: '../../../assets/icons/pdf.gif'
     },
     {
         id: '8',
         name: 'BEAUTY IN WHITE',
-        image: '../../../assets/ownerimage9.jpg'
+        image: '../../../assets/icons/pdf.gif'
     }
 ]
